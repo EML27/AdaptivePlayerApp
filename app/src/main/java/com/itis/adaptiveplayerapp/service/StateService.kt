eb9ad13ed81a682259578e3cb51c9cdd08c56bcf
Emil <emil.73@mail.ru>
2020-05-15 17:06:39 +0400

    empty service added

---
+++ app/src/main/java/com/itis/adaptiveplayerapp/service/StateService.kt
@@ -0,0 +1,74 @@
+package com.itis.adaptiveplayerapp.service
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.app.Service
+import android.content.Context
+import android.content.Intent
+import android.os.Binder
+import android.os.Build
+import android.os.IBinder
+import androidx.core.app.NotificationCompat
+import com.itis.adaptiveplayerapp.R
+
+class StateService : Service() {
+
+    private var state = "Calm"
+    val CHANNEL_ID = "27"
+    override fun onBind(intent: Intent): IBinder = mBinder
+
+    inner class MyBinder : Binder() {
+        fun getService() = this@StateService
+    }
+
+    var mBinder = MyBinder()
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        intent.let {
+            when (it?.action) {
+                "start" -> {setNotification()}
+            }
+        }
+        return super.onStartCommand(intent, flags, startId)
+    }
+
+    private fun setNotification() {
+        // Интенты для будущих команд
+        val getIntent =
+            { action: String -> Intent(this, StateService::class.java).setAction(action) }
+        val getPendingIntent =
+            { action: String -> PendingIntent.getService(this, 0, getIntent(action), 0) }
+
+        val currentStateTitle: String
+
+        //Логика установки текущего состояния
+        currentStateTitle = state
+        // оаоаоаоаоаоаоа
+        val notificationManager: NotificationManager =
+            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = getString(R.string.notification_channel_name)
+            val descriptionText = getString(R.string.notification_channel_description)
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel =
+                notificationManager.getNotificationChannel(CHANNEL_ID) ?: NotificationChannel(
+                    CHANNEL_ID,
+                    name,
+                    importance
+                ).apply {
+                    description = descriptionText
+                }
+            notificationManager.createNotificationChannel(channel)
+        }
+
+        val notification = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setContentTitle(currentStateTitle)
+            .setSmallIcon(R.drawable.ic_notification_small)
+            .build()
+
+        notificationManager.notify(1, notification)
+    }
+
+}
--- app/src/main/java/com/itis/adaptiveplayerapp/ui/view/MainActivity.kt
+++ app/src/main/java/com/itis/adaptiveplayerapp/ui/view/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.itis.adaptiveplayerapp.ui.view
 
+import android.content.ComponentName
+import android.content.Context
+import android.content.Intent
+import android.content.ServiceConnection
 import android.os.Bundle
+import android.os.IBinder
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.itis.adaptiveplayerapp.BuildConfig
 import com.itis.adaptiveplayerapp.R
 import com.itis.adaptiveplayerapp.bl.MusicRecommender
+import com.itis.adaptiveplayerapp.di.component.DaggerMusicRecommenderComponent
+import com.itis.adaptiveplayerapp.service.StateService
 import com.spotify.android.appremote.api.ConnectionParams
 import com.spotify.android.appremote.api.Connector.ConnectionListener
 import com.spotify.android.appremote.api.SpotifyAppRemote
@@ -17,23 +24,35 @@ class MainActivity : AppCompatActivity() {
 
     private var mSpotifyAppRemote: SpotifyAppRemote? = null
 
+    init {
+        DaggerMusicRecommenderComponent.create().inject(this)
+    }
+
     @Inject
     open lateinit var musicRecommender: MusicRecommender
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        bindService(
+            Intent(this, StateService::class.java),
+            mConnection,
+            Context.BIND_AUTO_CREATE
+        )
+        textView.text = musicRecommender.getRecommendedPlaylist()
+        //play_btn.setOnClickListener { mSpotifyAppRemote?.playerApi?.play("spotify:playlist:37i9dQZF1DWXpOtMyVOt4Q") }
 
-        //textView.text = musicRecommender.getRecommendedPlaylist()
-        play_btn.setOnClickListener { mSpotifyAppRemote?.playerApi?.play("spotify:playlist:37i9dQZF1DWXpOtMyVOt4Q") }
+        play_btn.setOnClickListener {
+            startService(Intent(this, StateService::class.java).apply { action = "start" })
+        }
     }
 
     override fun onStart() {
         super.onStart()
         var connectionParams = ConnectionParams.Builder(BuildConfig.CLIENT_ID)
-                .setRedirectUri(BuildConfig.API_REDIRECT)
-                .showAuthView(true)
-                .build()
+            .setRedirectUri(BuildConfig.API_REDIRECT)
+            .showAuthView(true)
+            .build()
 
         SpotifyAppRemote.connect(this, connectionParams,
             object : ConnectionListener {
@@ -54,9 +73,27 @@ class MainActivity : AppCompatActivity() {
     override fun onStop() {
         super.onStop()
         SpotifyAppRemote.disconnect(mSpotifyAppRemote)
+        stateService.let {
+            unbindService(mConnection)
+            stateService = null
+        }
     }
 
     private fun connected() {
 
+    }
+
+    var stateService: StateService? = null
+    private var mConnection = object : ServiceConnection {
+        override fun onServiceDisconnected(name: ComponentName?) {
+            stateService = null
+            //Toast.makeText(this@TrackActivity,"Entered onServiceDisconnected block", Toast.LENGTH_SHORT).show()
+        }
+
+        override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
+            val binder = service as StateService.MyBinder
+            stateService = binder.getService()
+            //Toast.makeText(this@TrackActivity,"Entered onServiceConnected block", Toast.LENGTH_SHORT).show()
+        }
     }
 }
